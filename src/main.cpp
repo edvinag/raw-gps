6a49401e14d66ce03ccdc66c3663199ba7715b9c
Edvin Agnas <edvin.agnas@gmail.com>
2024-11-21 07:38:10 +0100

    Add OTA

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,82 @@
 #include <Arduino.h>
 #include "WiFiManager.h"
 #include "OTAUpdateServer.h"
+#include <ArduinoOTA.h>
+#include "OTA.h"
 #include <WebServer.h>
 #include <Secrets.h>
 #include "WebServer.h"
 
+
+
 // OTA server configuration
-const char* host = "esp32-raw-gps";
+const char *host = "esp32-raw-gps";
 
 // Define GPIO pin to monitor
 const int enableOtaPin = 25; // Replace with the actual pin you are using
 
+unsigned long lastNMEATime = 0;
+
 // Create objects
-WiFiManager wifiManager(wifi_setup, sizeOfWifiSetup);;
+WiFiManager wifiManager(wifi_setup, sizeOfWifiSetup);
+;
 WebServer server(80); // Shared WebServer object
 OTAUpdateServer otaUpdateServer(server);
 bool otaEnabled = false; // Track OTA state
 
-void setup() {
-    Serial.begin(115200);
+void setup()
+{
+  Serial.begin(115200);
 
-    // Setup GPIO pin mode
-    pinMode(enableOtaPin, INPUT);
+  // Setup GPIO pin mode
+  pinMode(enableOtaPin, INPUT);
 
+  if (digitalRead(enableOtaPin) == HIGH)
+  {
+    Serial.println("Enabling OTA mode...");
     // Setup WiFi
     wifiManager.setup(host);
 
     // Setup OTA server
     otaUpdateServer.setup();
+    otaSetup();
 
     // Start the shared server
     server.begin();
-    Serial.println("Web server started");
-
-    // Initialize Serial and Serial1 at 9600 baud
+    otaEnabled = true;
+    Serial.println("Enabled OTA mode");
+  }
+  else
+  {
+    Serial.println("Enabling NMEA mode...");
     Serial1.begin(9600, SERIAL_8N1, 16, 17);
+  }
 }
 
-void loop() {
-    int pinState = digitalRead(enableOtaPin);
+void loop()
+{
+  if ((!otaEnabled && digitalRead(enableOtaPin) == HIGH) || (otaEnabled && digitalRead(enableOtaPin) == LOW)) // Restart ESP32 if OTA mode changes
+  {
+    Serial.println("Restarting ESP32...");
+    ESP.restart();
+  }
 
-    if (pinState == HIGH) {
-        if (!otaEnabled) {
-            Serial.println("Enabling OTA mode...");
-            otaEnabled = true;
-        }
-    } else {
-        if (otaEnabled) {
-            Serial.println("Disabling OTA mode...");
-            otaEnabled = false;
-        }
-    }
-
-    // Conditionally handle OTA server
-    if (otaEnabled) {
-        server.handleClient();
-    }
+  unsigned long currentMillis = millis();
+  if (otaEnabled) // Conditionally handle OTA server
+  {
+    server.handleClient();
+    ArduinoOTA.handle();
+  }
+  else if (currentMillis - lastNMEATime >= 200) // Send NMEA data every 200ms
+  {
+    lastNMEATime = currentMillis;
+    Serial1.println("$GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47");
+  }
+  else // Delay to prevent high CPU usage
+  {
+    delay(10);
+  }
 }
-
-
 
 // #include <Arduino.h>
 
